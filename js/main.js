fb52ef32334cd44ab525b4bb5fba54ee5f03fe7b
Waseh <wasehcheema@gmail.com>
2024-07-24 22:59:12 -0600

    Lab 3 complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const mainMenu = document.querySelector('.main-menu');
 
+    menuIcon.addEventListener('click', function() {
+        mainMenu.classList.toggle('active');
+    });
+});
